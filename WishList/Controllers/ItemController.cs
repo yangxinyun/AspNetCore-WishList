822424d00fdc6935537068d18ddf2801f8ec9c2d
yangxinyun <yangxinyun204@gmail.com>
2018-10-28 17:21:32 +0100

    Completed the module"

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext Dbcontext;
+        public ItemController(ApplicationDbContext dbcontext)
+        {
+            this.Dbcontext = dbcontext;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            Dbcontext.items.Add(item);
+            Dbcontext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var it = Dbcontext.items.FirstOrDefault(e => e.ID == id);
+            Dbcontext.items.Remove(it);
+            Dbcontext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            var model = Dbcontext.items.ToList();
+            return View("Index",model);
+        }
+    }
+}
