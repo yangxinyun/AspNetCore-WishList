f2a7ac7c64a22e40b16c2b4de65c4a6cca4db3c8
yangxinyun <yangxinyun204@gmail.com>
2018-10-28 16:06:32 +0100

    "Completed the module"

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Item> items{ get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
